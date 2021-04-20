d1aee17ff6ad88f81c3fa3c886de670b4a82c773
Noupin <naperkins7@gmail.com>
2021-04-20 15:11:31 -0400

    Remove buttons in media list are now per element instead of per isntance of medi list

--- src/Components/MediaList/MediaList.tsx
+++ src/Components/MediaList/MediaList.tsx
@@ -1,12 +1,32 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 //Third Party Imports
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import { Prev } from 'react-bootstrap/esm/PageItem';
 import { IElevatedPageState } from '../../Interfaces/PageState';
 import { Button } from '../Button/Button';
 import { Media } from '../Media/Media';
 
+
+var hiddenButtonStyle: React.CSSProperties = {position: 'absolute',
+                                              top: "50%",
+                                              left: "50%",
+                                              transform: "translate(-50%, -50%)",
+                                              width: "60%",
+                                              height: "60%",
+                                              border: "none",
+                                              fontSize: "25px",
+                                              display: "none",
+                                              fontWeight: 'bolder'}
+var shownButtonStyle: React.CSSProperties = {position: 'absolute',
+                                             top: "50%",
+                                             left: "50%",
+                                             transform: "translate(-50%, -50%)",
+                                             width: "60%",
+                                             height: "60%",
+                                             border: "none",
+                                             fontSize: "25px",
+                                             display: "flex",
+                                             fontWeight: 'bolder'}
 
 interface IMediaList extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>{
   setElevatedState: React.Dispatch<React.SetStateAction<IElevatedPageState>>
@@ -22,29 +42,6 @@ export function MediaList(props: IMediaList){
   var elements: (React.ReactChild | React.ReactFragment | React.ReactPortal)[] = [];
   let gridElements: React.ReactNode[][] = [];
   let gridRow = -1;
-  const [buttonStyle, setButtonStyle] = useState<React.CSSProperties>(
-                                          {position: 'absolute',
-                                           top: "50%",
-                                           left: "50%",
-                                           transform: "translate(-50%, -50%)",
-                                           width: "60%",
-                                           height: "60%",
-                                           border: "none",
-                                           display: "none",
-                                           fontSize: "25px",
-                                           fontWeight: 'bolder'});
-
-  function showDeleteButton(){
-    setButtonStyle((prev) => ({...prev, display: 'flex'}));
-  }
-
-  function hideDeleteButton(){
-    setButtonStyle((prev) => ({...prev, display: 'none'}));
-  }
-  
-  function removeElement(itemList: File[], setItemList: React.Dispatch<React.SetStateAction<File[]>>, deleteIndex: number){
-    setItemList(itemList.filter((item, index: number) => index !== deleteIndex))
-  }
 
 
   if(mediaArray){
@@ -58,6 +55,20 @@ export function MediaList(props: IMediaList){
     elements = React.Children.toArray(children);
   }
 
+  const [deleteButtonVisible, setDeleteButtonVisible] = useState(Array(elements.length).fill(false));
+
+  function showDeleteButton(changeIndex: number){
+    setDeleteButtonVisible((prev) => prev.map((item, index) => index !== changeIndex ? item : true))
+  }
+
+  function hideDeleteButton(changeIndex: number){
+    setDeleteButtonVisible((prev) => prev.map((item, index) => index !== changeIndex ? item : false))
+  }
+  
+  function removeElement(itemList: File[], setItemList: React.Dispatch<React.SetStateAction<File[]>>, deleteIndex: number){
+    setItemList(itemList.filter((item, index: number) => index !== deleteIndex))
+  }
+
   for(let elementCounter = 0; elementCounter < elements.length; elementCounter++){
     if(elementCounter % elementsPerRow === 0){
       gridRow++;
@@ -66,17 +77,26 @@ export function MediaList(props: IMediaList){
     gridElements[gridRow] = [...gridElements[gridRow], elements[elementCounter]]
   }
 
+  useEffect(() => {
+    setDeleteButtonVisible(deleteButtonVisible);
+  }, [deleteButtonVisible])
+
 
   return (
     <div {...mediaListProps} className={cssClasses}>
-      {gridElements.map((row, rowIndex) => (
+      {gridElements.map((row, rowIndex: number) => (
         <Row className="my-2 mx-0" key={rowIndex}>
-          {row.map((element, colIndex) => (
+          {row.map((element, colIndex: number) => (
             <Col className="align-middle relative" key={row.indexOf(element)}
-                 onMouseEnter={() => showDeleteButton()}
-                 onMouseLeave={() => hideDeleteButton()}>
+                 onMouseEnter={() => showDeleteButton((rowIndex*elementsPerRow)+colIndex)}
+                 onMouseLeave={() => hideDeleteButton((rowIndex*elementsPerRow)+colIndex)}>
               {element}
-              <Button style={buttonStyle} className="glassmorphic borderRadius-2 justify-content-center" onClick={() => removeElement(mediaArray!, setMediaArray!, (rowIndex*elementsPerRow)+colIndex)}>&#x2715;</Button>
+
+              <Button style={deleteButtonVisible[(rowIndex*elementsPerRow)+colIndex] ? shownButtonStyle : hiddenButtonStyle}
+                      className="glassmorphic borderRadius-2 justify-content-center"
+                      onClick={() => removeElement(mediaArray!, setMediaArray!, (rowIndex*elementsPerRow)+colIndex)}>
+                        &#x2715;
+              </Button>
             </Col>
           ))}
         </Row>
