05b3cab272614d1c082a2bd1d8b8ef50367c26fa
Noupin <naperkins7@gmail.com>
2021-05-31 15:07:04 -0400

    Updated shift page

--- src/Modules/ShiftPage.tsx
+++ src/Modules/ShiftPage.tsx
@@ -2,26 +2,34 @@
 
 //Third Party Imports
 import React, { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
-import { useParams } from 'react-router';
+import { Container, Col, Row } from 'react-bootstrap';
+import { useHistory, useParams } from 'react-router';
 
 //First Party Imports
 import { ShiftAPIInstance } from '../Helpers/Api';
 import { IElevatedStateProps } from '../Interfaces/ElevatedStateProps';
 import { IndividualShiftGetResponse } from '../Swagger/models/IndividualShiftGetResponse';
-import { GetIndivdualShiftRequest, Shift } from '../Swagger';
+import { GetIndivdualShiftRequest } from '../Swagger';
 import { Media } from '../Components/Media/Media';
+import { Image } from '../Components/Image/Image';
+import { Button } from '../Components/Button/Button';
+import Verified from "../Assets/verified_checkmark.svg";
+import Admin from "../Assets/admin.svg";
+import LeftCurvedArrow from "../Assets/LeftCurvedArrow.svg"
+import RightCurvedArrow from "../Assets/RightCurvedArrow.svg"
 
 
 export function ShiftPage (props: IElevatedStateProps){
   const {setElevatedState} = props;
+  const history = useHistory()
 
   let { uuid } = useParams<GetIndivdualShiftRequest>();
 
 
   const [shiftResponse, setShiftResponse] = useState<IndividualShiftGetResponse>();
-  const [shift, setShift] = useState<Shift>()
   const [shiftMediaURL, setShiftMediaURL] = useState("");
+  const [baseMediaURL, setBaseMediaURL] = useState("");
+  const [maskMediaURL, setMaskMediaURL] = useState("");
   
   
   useEffect(() => {
@@ -37,14 +45,97 @@ export function ShiftPage (props: IElevatedStateProps){
   useEffect(() => {
     if (!shiftResponse) return;
 
-    setShift(shiftResponse.shift)
     setShiftMediaURL(`/api/content/image/${shiftResponse.shift!.mediaFilename!}`)
+    setBaseMediaURL(`/api/content/image/${shiftResponse.shift!.baseMediaFilename!}`)
+    setMaskMediaURL(`/api/content/image/${shiftResponse.shift!.maskMediaFilename!}`)
   }, [shiftResponse])
+
+  let userComponent = <></>
+  let shiftTitleComponent = <></>
+  let updateShiftComponent = <></>
+  if(shiftResponse){
+    shiftTitleComponent = (
+      <h1>
+        {shiftResponse.shift!.title} {shiftResponse.shift!.verified ?
+        <Image style={{height: "0.75em", width: "auto"}} 
+            className="object-fit-contain"
+            imageSrc={Admin} alt="Admin"/> : <></>}
+      </h1>
+    )
+    if(shiftResponse.owner){
+      updateShiftComponent = (
+        <Row>
+          <Button className="borderRadius-2 p-2 mt-2">Edit Shift</Button>
+        </Row>
+      )
+    }
+    userComponent = (
+      <div onClick={() => history.push(`/user/${shiftResponse.shift!.author.username}`)}
+        style={{cursor: "pointer"}}>
+        <Row>
+          <h4>
+            {shiftResponse.shift!.author.username} {shiftResponse.shift!.author.verified! ?
+            <Image style={{height: "0.75em", width: "auto"}} 
+                className="object-fit-contain"
+                imageSrc={Admin} alt="Admin"/> : <></>} {shiftResponse.shift!.author.admin! ?
+            <Image style={{height: "0.75em", width: "auto"}} 
+                className="object-fit-contain" imageSrc={Verified} alt="Verified"/> : <></>}
+          </h4>
+        </Row>
+        <Row>
+          <Media className="neumorphic borderRadius-3 p-2"
+                 srcString={`/api/content/image/${shiftResponse.shift!.author.mediaFilename!}`}
+                 setElevatedState={setElevatedState}/>
+        </Row>
+      </div>
+    )
+  }
 
   return (
     <Container>
-      <h1>{shift ? shift!.title : ""}</h1>
-      <Media className="neumorphic borderRadius-3 p-2" srcString={shiftMediaURL} setElevatedState={setElevatedState}/>
+      <Row className="justify-content-center">
+        {shiftTitleComponent}
+      </Row>
+      <Row>
+        <Col xs={3} className="p-2">
+          {userComponent}
+          {updateShiftComponent}
+        </Col>
+        <Col xs={9} className="p-2">
+          <Row>
+            <Col xs={2}></Col>
+            <Col xs={8}>
+              <Media className="neumorphic borderRadius-3 p-2" srcString={shiftMediaURL}
+                setElevatedState={setElevatedState}/>
+            </Col>
+            <Col xs={2}></Col>
+          </Row>
+          <Row>
+            <Col xs={2}></Col>
+            <Col xs={4}>
+              <Image style={{height: "5em", width: "auto"}} className="m-1"
+                imageSrc={LeftCurvedArrow} alt="LeftArrow"/>
+            </Col>
+            <Col xs={4}>
+              <Image style={{height: "5em", width: "auto"}} className="m-1"
+                imageSrc={RightCurvedArrow} alt="RightArrow"/>
+            </Col>
+            <Col xs={2}></Col>
+          </Row>
+          <Row>
+            <Col xs={1}></Col>
+            <Col xs={5}>
+              <Media className="neumorphic borderRadius-3 p-2" srcString={baseMediaURL}
+                setElevatedState={setElevatedState}/>
+            </Col>
+            <Col xs={5}>
+              <Media className="neumorphic borderRadius-3 p-2" srcString={maskMediaURL}
+                setElevatedState={setElevatedState}/>
+            </Col>
+            <Col xs={1}></Col>
+          </Row>
+        </Col>
+      </Row>
     </Container>
   );
 }
